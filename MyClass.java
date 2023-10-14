b27b5f303d15e068a98412fdf41a7968946493df
Asmit Aditya Singh <125098141+Asmit13@users.noreply.github.com>
2023-10-14 22:44:36 +0530

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,15 @@
+public class MyClass {
+    private int myField;
+
+    public MyClass(int initialValue) {
+        myField = initialValue;
+    }
+
+    public int getMyField() {
+        return myField;
+    }
+
+    public void setMyField(int value) {
+        myField = value;
+    }
+}
